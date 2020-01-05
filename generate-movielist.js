59bbc72c08854f7858671d17a02d2dbd89ce8a92
Matthew Roseman <mrr77@case.edu>
2020-01-05 18:18:12 -0500

    fixed async bug in generate-movielist where imdb file was read before it was written
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- generate-movielist.js
+++ generate-movielist.js
@@ -7,23 +7,24 @@ const unzip = zlib.createUnzip();
 
 const imdbFileOutStream = fs.createWriteStream('./data/title.basics.tsv', 'utf8');
 https.get('https://datasets.imdbws.com/title.basics.tsv.gz', (res) => {
-  res.pipe(unzip).pipe(imdbFileOutStream);
+  res.pipe(unzip).pipe(imdbFileOutStream).on('finish', () => {
+    const fileInStream = fs.createReadStream('./data/title.basics.tsv', 'utf8');
+    const fileOutStream = fs.createWriteStream('./data/movieTitles.txt', {flags: 'w'});
+
+    const rl = readline.createInterface({
+      input: fileInStream,
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+      const data = line.split('\t');
+
+      if (data.length >= 2 && data[1] === 'movie') {
+        if (data.length >= 3 && data[2] !== '\\N') {
+          fileOutStream.write(data[2] + '\n');
+        }
+      }
+    });
+  });
 });
 
-const fileInStream = fs.createReadStream('./data/title.basics.tsv', 'utf8');
-const fileOutStream = fs.createWriteStream('./data/movieTitles.txt', {flags: 'w'});
-
-const rl = readline.createInterface({
-  input: fileInStream,
-  crlfDelay: Infinity
-});
-
-rl.on('line', (line) => {
-  const data = line.split('\t');
-
-  if (data.length >= 2 && data[1] === 'movie') {
-    if (data.length >= 3 && data[2] !== '\\N') {
-      fileOutStream.write(data[2] + '\n');
-    }
-  }
-});
