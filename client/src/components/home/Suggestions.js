ed85833201efa27b47f1c911ac37a0944991a26a
Matthew Roseman <mrr77@case.edu>
2020-01-27 18:55:49 -0500

    converted Suggestions to a function component instead of class

--- client/src/components/home/Suggestions.js
+++ client/src/components/home/Suggestions.js
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './Suggestions.css';
 
-export default class Suggestions extends React.Component {
-  componentDidUpdate() {
-    const movieTitleField = document.getElementById('movie-title-field');
-    const suggestionMenuElement = document.getElementById('suggestion-menu-container');
+// element selectors that are used throughout Suggestions
+let movieTitleField;
+let suggestionMenu;
 
+export default function Suggestions(props) {
+  // initialize the element selectors when the componenet first loads
+  useEffect(() => {
+    movieTitleField = document.getElementById('movie-title-field');
+    suggestionMenu = document.getElementById('suggestion-menu-container');
+  }, []);
+
+  // whenever the suggestion menu is shown, make sure it's lined up underneath the movieTitleField
+  useEffect(() => {
     const styleTop = `${movieTitleField.getBoundingClientRect().height}px`;
     const styleLeft = '0px';
     const styleWidth = `${movieTitleField.getBoundingClientRect().width}px`;
 
-    suggestionMenuElement.style.top = styleTop;
-    suggestionMenuElement.style.left = styleLeft;
-    suggestionMenuElement.style.maxWidth = styleWidth;
-    suggestionMenuElement.style.minWidth = styleWidth;
-  }
+    suggestionMenu.style.top = styleTop;
+    suggestionMenu.style.left = styleLeft;
+    suggestionMenu.style.maxWidth = styleWidth;
+    suggestionMenu.style.minWidth = styleWidth;
+  }, [props.show]);
 
-  render() {
-    const suggestionMenuElement = document.getElementById('suggestion-menu-container');
-    if (suggestionMenuElement && suggestionMenuElement.classList.contains('hidden') && this.props.show) {
-      // if the show prop changed to show, animate the suggestion menu dropping down
-      suggestionMenuElement.style.maxHeight = '0px';
-      suggestionMenuElement.style.padding = '0px';
+  // when showing the suggestion menu, start it at no height, and then immediately switch to actual
+  // height to trigger the opening animation
+  useEffect(() => {
+    // if the suggestion menu should show, and is currently hidden, animate it dropping down
+    if (props.show && suggestionMenu.classList.contains('hidden')) {
+      suggestionMenu.style.maxHeight = '0px';
+      suggestionMenu.style.padding = '0px';
+
       setTimeout(() => {
         if (window.innerWidth <= 575) {
-          suggestionMenuElement.style.maxHeight = `${window.innerHeight - suggestionMenuElement.getBoundingClientRect().top - 20}px`;
+          suggestionMenu.style.maxHeight = `${window.innerHeight - suggestionMenu.getBoundingClientRect().top - 20}px`;
         } else {
-          suggestionMenuElement.style.maxHeight = null;
+          suggestionMenu.style.maxHeight = null;
         }
-        suggestionMenuElement.style.padding = null;
+
+        suggestionMenu.style.padding = null;
       }, 10);
     }
+  }, [props.show]);
 
-    return (
-      <div
-        id="suggestion-menu-container"
-        className={this.props.show ? '' : 'hidden'}
-      >
-        <div id="suggestion-menu">
-          {this.props.suggestions.map((suggestion) => {
-            return (
-              <div
-                key={suggestion}
-                className='suggestion'
-                onMouseDown={(event) => this.props.onSuggestionClick(event, suggestion)}
-              >
-                {suggestion}
-              </div>
-            );
-          })}
-        </div>
+  return (
+    <div
+      id="suggestion-menu-container"
+      className={props.show ? '' : 'hidden'}
+    >
+      <div id="suggestion-menu">
+        {props.suggestions.map((suggestion) => {
+          return (
+            <div
+              key={suggestion}
+              className='suggestion'
+              onMouseDown={(event) => props.onSuggestionClick(event, suggestion)}
+            >
+              {suggestion}
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
