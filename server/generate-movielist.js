cd3bcbd99cf7410e267b77fa1e1e883ccb1b5d9d
Matthew Roseman <mrr77@case.edu>
2020-01-31 21:40:46 -0500

    added timing logs throughout generate movielist

--- server/generate-movielist.js
+++ server/generate-movielist.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 
 const { Movie } = require('./models.js');
 
+const globalStartTime = new Date();
+
 // load database environment variables
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
@@ -31,15 +33,23 @@ mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
 function downloadBasicsFile() {
   console.log('downloading IMDb basics file');
 
+  const startTime = new Date();
+
   const imdbBasicsFileOutStream = fs.createWriteStream('./data/title.basics.tsv', {flags: 'w'});
   const unzip = zlib.createUnzip();
   https.get('https://datasets.imdbws.com/title.basics.tsv.gz', (res) => {
-    res.pipe(unzip).pipe(imdbBasicsFileOutStream).on('finish', processBasicsFile);
+    res.pipe(unzip).pipe(imdbBasicsFileOutStream).on('finish', () => {
+      const timeTook = Math.round((new Date() - startTime) / 1000);
+      console.log(`IMDb basics file downloaded. took: ${timeTook}s`);
+      processBasicsFile();
+    });
   });
 }
 
 function processBasicsFile() {
   console.log('processing IMDb basics file');
+
+  let startTime = new Date();
 
   const imdbBasicsFileInStream = fs.createReadStream('./data/title.basics.tsv', 'utf8');
 
@@ -72,11 +82,20 @@ function processBasicsFile() {
   });
 
   rl.on('close', () => {
+    let timeTook = Math.round((new Date() - startTime) / 1000);
+    console.log(`IMDb basics file processed. took: ${timeTook}s`);
+
     console.log('bulk writing movie upserts');
+
+    startTime = new Date();
+
     Movie.bulkWrite(upsertOperations)
       .then((res) => {
         console.log(`${res.upsertedCount} new documents added`);
         console.log(`${res.modifiedCount} documents updated`);
+
+        timeTook = Math.round((new Date() - startTime) / 1000);
+        console.log(`took: ${timeTook}s`);
 
         downloadRatingsFile();
       })
@@ -89,15 +108,24 @@ function processBasicsFile() {
 function downloadRatingsFile() {
   console.log('downloading IMDb ratings file');
 
+  const startTime = new Date();
+
   const imdbRatingsFileOutStream = fs.createWriteStream('./data/title.ratings.tsv', {flags: 'w'});
   const unzip = zlib.createUnzip();
   https.get('https://datasets.imdbws.com/title.ratings.tsv.gz', (res) => {
-    res.pipe(unzip).pipe(imdbRatingsFileOutStream).on('finish', processRatingsFile);
+    res.pipe(unzip).pipe(imdbRatingsFileOutStream).on('finish', () => {
+      const timeTook = Math.round((new Date() - startTime) / 1000);
+      console.log(`IMDb ratings file downloaded. took: ${timeTook}s`);
+
+      processRatingsFile();
+    });
   });
 }
 
 function processRatingsFile() {
   console.log('processing IMDb ratings file');
+
+  let startTime = new Date();
 
   const imdbRatingsFileInStream = fs.createReadStream('./data/title.ratings.tsv', 'utf8');
 
@@ -114,6 +142,9 @@ function processRatingsFile() {
     const rating = isFinite(parseInt(data[1])) ? parseInt(data[1]) : null;
     const numVotes = isFinite(parseInt(data[2])) ? parseInt(data[2]) : null;
 
+    // TODO it may be faster to do a query by id here and only add the upserts that will actually update
+    // TODO a bunch of id exists queries may be faster than the one big bulkWrite query
+    // TODO could also store list of ids from first part to save time here
     upsertOperations.push({
       updateOne: {
         filter: { _id: id },
@@ -123,13 +154,23 @@ function processRatingsFile() {
   });
 
   rl.on('close', () => {
+    let timeTook = Math.round((new Date() - startTime) / 1000);
+    console.log(`IMDb ratings file downloaded. took: ${timeTook}s`);
+
     console.log('bulk writing movie upserts');
+
+    startTime = new Date();
+
     Movie.bulkWrite(upsertOperations)
       .then((res) => {
         console.log(`${res.upsertedCount} new documents added`);
         console.log(`${res.modifiedCount} documents updated`);
 
-        console.log('finished generating movie list');
+        timeTook = Math.round((new Date() - startTime) / 1000);
+        console.log(`movie list generated. took ${timeTook}`);
+
+        timeTook = Math.round((new Date() - globalStartTime) / 1000);
+        console.log(`total time: ${timeTook}s`);
       })
       .catch((err) => {
         console.error(`error adding documents: ${err}`);
