34f96059fdabf3e21c843f06b0a0c6671a2ec818
Matthew Roseman <mrr77@case.edu>
2020-02-01 02:15:30 -0500

    refactored generate-movielist to by more asyncronous and efficient

--- server/generate-movielist.js
+++ server/generate-movielist.js
@@ -1,12 +1,17 @@
 const zlib = require('zlib');
 const https = require('https');
-const fs = require('fs');
 const readline = require('readline');
+
 const mongoose = require('mongoose');
 
 const { Movie } = require('./models.js');
 
-const globalStartTime = new Date();
+
+const IMDB_BASIC_FILE_URL = 'https://datasets.imdbws.com/title.basics.tsv.gz';
+const IMDB_RATING_FILE_URL = 'https://datasets.imdbws.com/title.ratings.tsv.gz';
+
+let preExistingMovieIds;
+let movieIds;
 
 // load database environment variables
 const DB_HOST = process.env.DB_HOST;
@@ -17,163 +22,239 @@ const DB_NAME = process.env.DB_NAME;
 
 // config mongoose
 mongoose.set('useFindAndModify', false);
-
-// connect to database
 const mongoUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
+
+/*
+ * addAllMoviesToDb downloads a list of current movies from IMDb, including data associated with the movies.
+ * Then it adds all the movies to mongoDB.
+ * @param checkForUpdates: if true will update all preexisting documents in the db.
+ */
+async function addAllMoviesToDb() {
+  const startTime = new Date();
+
+  // connect to database
+  try {
+    await mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
     console.log('connected to database');
-    downloadBasicsFile();
-    // processRatingsFile();
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error('connection error: ' + err);
-  });
+  }
 
-function downloadBasicsFile() {
-  console.log('downloading IMDb basics file');
+  preExistingMovieIds = new Set(await Movie.getAllIds());
+  console.log(`already ${preExistingMovieIds.size} movies in db`);
 
-  const startTime = new Date();
+  // add movie info to db
+  try {
+    await addBasicInfoToDb();
+  } catch (err) {
+    console.error(`there was an error adding basic movie info to db: ${err}`);
+  }
 
-  const imdbBasicsFileOutStream = fs.createWriteStream('./data/title.basics.tsv', {flags: 'w'});
-  const unzip = zlib.createUnzip();
-  https.get('https://datasets.imdbws.com/title.basics.tsv.gz', (res) => {
-    res.pipe(unzip).pipe(imdbBasicsFileOutStream).on('finish', () => {
-      const timeTook = Math.round((new Date() - startTime) / 1000);
-      console.log(`IMDb basics file downloaded. took: ${timeTook}s`);
-      processBasicsFile();
-    });
-  });
+  movieIds = new Set(await Movie.getAllIds());
+
+  try {
+    await addRatingInfoToDb();
+  } catch (err) {
+    console.error(`there was an error updating movie rating info in db: ${err}`);
+  }
+
+  console.log('All movie data has been downloaded and updated');
+
+  console.log('took: ' + Math.round(new Date() - startTime) / 1000 + 's');
+
+  return;
 }
 
-function processBasicsFile() {
-  console.log('processing IMDb basics file');
+/*
+ * addBasicInfoToDb downloads the basic info from IMDb, and adds all basic movie info to the database
+ */
+async function addBasicInfoToDb() {
+  return new Promise((resolve, reject) => {
+    console.log('downloading and processing IMDb basics file');
 
-  let startTime = new Date();
+    downloadBasicInfo().then((rl) => {
+      const movieDocuments = [];
 
-  const imdbBasicsFileInStream = fs.createReadStream('./data/title.basics.tsv', 'utf8');
+      rl.on('line', async (line) => {
+        const basicInfo = processBasicInfo(line);
 
-  const rl = readline.createInterface({
-    input: imdbBasicsFileInStream,
-    crlfDelay: Infinity
-  });
-
-  const upsertOperations = [];
-
-  rl.on('line', (line) => {
-    const data = line.split('\t');
-
-    // if this isn't a movie
-    if (data[1] !== 'movie') {
-      return;
-    }
-
-    const id = data[0];
-    let title = data[2];
-    const year = isFinite(parseInt(data[5])) ? parseInt(data[5]) : null;
-
-    upsertOperations.push({
-      updateOne: {
-        filter: { _id: id },
-        update: { title: title, year: year },
-        upsert: true
-      }
-    });
-  });
-
-  rl.on('close', () => {
-    let timeTook = Math.round((new Date() - startTime) / 1000);
-    console.log(`IMDb basics file processed. took: ${timeTook}s`);
-
-    console.log('bulk writing movie upserts');
-
-    startTime = new Date();
-
-    Movie.bulkWrite(upsertOperations)
-      .then((res) => {
-        console.log(`${res.upsertedCount} new documents added`);
-        console.log(`${res.modifiedCount} documents updated`);
-
-        timeTook = Math.round((new Date() - startTime) / 1000);
-        console.log(`took: ${timeTook}s`);
-
-        downloadRatingsFile();
-      })
-      .catch((err) => {
-        console.error(`error adding documents: ${err}`);
+        // if this line was successfully processed, add it to the upsertOperations array
+        if (basicInfo) {
+          movieDocuments.push({
+            _id: basicInfo.id,
+            title: basicInfo.title,
+            year: basicInfo.year
+          });
+        }
       });
-  });
-}
 
-function downloadRatingsFile() {
-  console.log('downloading IMDb ratings file');
+      rl.on('close', async () => {
+        console.log('IMDb basics file downloaded and processed');
 
-  const startTime = new Date();
+        if (movieDocuments.length === 0) {
+          console.log('no new movies to add to db');
 
-  const imdbRatingsFileOutStream = fs.createWriteStream('./data/title.ratings.tsv', {flags: 'w'});
-  const unzip = zlib.createUnzip();
-  https.get('https://datasets.imdbws.com/title.ratings.tsv.gz', (res) => {
-    res.pipe(unzip).pipe(imdbRatingsFileOutStream).on('finish', () => {
-      const timeTook = Math.round((new Date() - startTime) / 1000);
-      console.log(`IMDb ratings file downloaded. took: ${timeTook}s`);
+          resolve();
+          return;
+        }
 
-      processRatingsFile();
-    });
-  });
-}
+        console.log('adding basic movie info to db');
+        try {
+          await Movie.insertMany(movieDocuments);
 
-function processRatingsFile() {
-  console.log('processing IMDb ratings file');
+          console.log(`${movieDocuments.length} new documents added`);
+        } catch (err) {
+          reject(`error adding documents: ${err}`);
+        }
 
-  let startTime = new Date();
-
-  const imdbRatingsFileInStream = fs.createReadStream('./data/title.ratings.tsv', 'utf8');
-
-  const rl = readline.createInterface({
-    input: imdbRatingsFileInStream,
-    crlfDelay: Infinity
-  });
-
-  const upsertOperations = [];
-
-  rl.on('line', (line) => {
-    const data = line.split('\t');
-    const id = data[0];
-    const rating = isFinite(parseInt(data[1])) ? parseInt(data[1]) : null;
-    const numVotes = isFinite(parseInt(data[2])) ? parseInt(data[2]) : null;
-
-    // TODO it may be faster to do a query by id here and only add the upserts that will actually update
-    // TODO a bunch of id exists queries may be faster than the one big bulkWrite query
-    // TODO could also store list of ids from first part to save time here
-    upsertOperations.push({
-      updateOne: {
-        filter: { _id: id },
-        update: { rating: rating, numVotes: numVotes },
-      }
-    });
-  });
-
-  rl.on('close', () => {
-    let timeTook = Math.round((new Date() - startTime) / 1000);
-    console.log(`IMDb ratings file downloaded. took: ${timeTook}s`);
-
-    console.log('bulk writing movie upserts');
-
-    startTime = new Date();
-
-    Movie.bulkWrite(upsertOperations)
-      .then((res) => {
-        console.log(`${res.upsertedCount} new documents added`);
-        console.log(`${res.modifiedCount} documents updated`);
-
-        timeTook = Math.round((new Date() - startTime) / 1000);
-        console.log(`movie list generated. took ${timeTook}`);
-
-        timeTook = Math.round((new Date() - globalStartTime) / 1000);
-        console.log(`total time: ${timeTook}s`);
-      })
-      .catch((err) => {
-        console.error(`error adding documents: ${err}`);
+        resolve();
       });
+    });
   });
 }
+
+/*
+ * downloadBasicInfo queries IMDb to get the basiv movie info file, uncompresses it and returns a
+ * readline interface that'll fire a 'line' event for each line of the file.
+ * @return: a readline interface for the downloaded file
+ */
+async function downloadBasicInfo() {
+  return new Promise((resolve, reject) => {
+    const unzip = zlib.createUnzip();
+
+    https.get(IMDB_BASIC_FILE_URL, (res) => {
+      resolve(readline.createInterface({
+        input: res.pipe(unzip),
+        crlfDelay: Infinity
+      }));
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+/*
+ * processBasicInfo takes a line from the IMDb basics info uncompressed download, grabs the relevant data
+ * and returns it as an object
+ * @param line: a string that's a tab seperated line from IMDb basics file
+ * @return: an object that is the relevant data parsed out of the given line, null if the given line isn't a movie
+ */
+function processBasicInfo(line) {
+  const data = line.split('\t');
+
+  // if this isn't a movie
+  if (data[1] !== 'movie') {
+    return null;
+  }
+
+  const id = data[0];
+  let title = data[2];
+  const year = isFinite(parseInt(data[5])) ? parseInt(data[5]) : null;
+
+  if (preExistingMovieIds.has(id)){
+    return null;
+  }
+
+  return {
+    id,
+    title,
+    year
+  };
+}
+
+/*
+ * addRatingInfoToDb downloads the rating info from IMDb, and adds all movie rating info to the database
+ */
+async function addRatingInfoToDb() {
+  return new Promise((resolve, reject) => {
+    console.log('downloading and processing IMDb ratings file');
+
+    downloadRatingInfo().then((rl) => {
+      const updateOperations = [];
+
+      rl.on('line', async (line) => {
+        const ratingInfo = processRatingInfo(line);
+
+        // if this line was successfully processed, add it to the upsertOperations array
+        if (ratingInfo) {
+          updateOperations.push({
+            updateOne: {
+              filter: { _id: ratingInfo.id },
+              update: { numVotes: ratingInfo.numVotes, rating: ratingInfo.rating }
+            }
+          });
+        }
+      });
+
+      rl.on('close', async () => {
+        console.log('IMDb ratings file downloaded and processed');
+
+        if (updateOperations.length === 0) {
+          console.log('no new movie ratings to add to db');
+
+          resolve();
+          return;
+        }
+
+        console.log('updating movie ratings in db');
+        try {
+          const res = await Movie.bulkWrite(updateOperations)
+
+          console.log(`${res.modifiedCount} documents updated`);
+        } catch (err) {
+          reject(`error adding documents: ${err}`);
+        }
+
+        resolve();
+      });
+    });
+  });
+}
+
+/*
+ * downloadRatingInfo queries IMDb to get the movie ratings info file, uncompresses it and returns a
+ * readline interface that'll fire a 'line' event for each line of the file.
+ * @return: a readline interface for the downloaded file
+ */
+async function downloadRatingInfo() {
+  return new Promise((resolve, reject) => {
+    const unzip = zlib.createUnzip();
+
+    https.get(IMDB_RATING_FILE_URL, (res) => {
+      resolve(readline.createInterface({
+        input: res.pipe(unzip),
+        crlfDelay: Infinity
+      }));
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+/*
+ * processRatingInfo takes a line from the IMDb movies ratings info uncompressed download, grabs the relevant data
+ * and returns it as an object
+ * @param line: a string that's a tab seperated line from IMDb ratings file
+ * @return: an object that is the relevant data parsed out of the given line, null if the movie id isn't in the database
+ */
+function processRatingInfo(line) {
+  const data = line.split('\t');
+  const id = data[0];
+  const rating = isFinite(parseInt(data[1])) ? parseInt(data[1]) : null;
+  const numVotes = isFinite(parseInt(data[2])) ? parseInt(data[2]) : null;
+
+  // if there isn't a movie for the current rating in the database, return null
+  if (!movieIds.has(id)) {
+    return null;
+  }
+
+  return {
+    id,
+    rating,
+    numVotes
+  };
+}
+
+(async () => {
+  await addAllMoviesToDb();
+})();
--- server/models.js
+++ server/models.js
@@ -13,6 +13,14 @@ movieSchema.methods.toString = function() {
   return `Movie: (_id: ${this._id}, title: ${this.title}, year: ${this.year}, numVotes: ${this.numVotes}, rating: ${this.rating})`;
 };
 
+movieSchema.statics.getAllIds = async function() {
+  return await this.find().distinct('_id');
+};
+
+movieSchema.statics.idExists = async function(id) {
+  return await this.exists({_id: id});
+};
+
 const Movie = mongoose.model('Movie', movieSchema);
 
 module.exports.Movie = Movie;
