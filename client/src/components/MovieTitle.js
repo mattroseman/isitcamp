4668907e825d6da80b6eeeca7c69bdacdded6576
Matt <mroseman95@gmail.com>
2020-10-02 19:34:13 -0400

    Styled the movie title with smaller font for longer movie titles

--- client/src/components/MovieTitle.js
+++ client/src/components/MovieTitle.js
@@ -5,7 +5,7 @@ import './MovieTitle.css';
 
 export default function MovieTitle(props) {
   return (
-    <h1 id="movie-title">
+    <h1 id='movie-title' className={props.title.length > 24 ? 'long-title' : ''}>
       {props.title}
     </h1>
   );
